0bfdb263274466d6babc00b2bea4e07c3365fd15
Roman Shatilov <ownspaze@ya.ru>
2023-09-04 14:51:58 +0300

    Изменения

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld newBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(newBean.getMessage());
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        Cat beanNewCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanNewCat.getMessage());
+
+        boolean equalsHelloWorldBean = (bean.equals(newBean));
+        boolean equalsCatBean = (beanCat.equals(beanNewCat));
+
+        System.out.println(equalsHelloWorldBean);
+        System.out.println(equalsCatBean);
     }
 }
